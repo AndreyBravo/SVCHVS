f1c2b2954cd70063ddf4b1f82a57125efa4bc236
Andrey Babichev <klopipa@gmail.com>
2023-02-26 23:38:53 +0300

    init script code

---
+++ src/script.js
@@ -0,0 +1,31 @@
+// Выбираем кнопку
+const btn = document.querySelector(".btn-toggle");
+// Выбираем настройки темы из localStorage
+const currentTheme = localStorage.getItem("theme");
+// Если текущая тема в localStorage равна "dark"…
+if (currentTheme == "dark") {
+  // …тогда мы используем класс .dark-theme
+  document.body.classList.add("dark-theme");
+}
+
+// Отслеживаем щелчок по кнопке
+btn.addEventListener("click", function () {
+  // Переключаем класс .dark-theme при каждом щелчке
+  document.body.classList.toggle("dark-theme");
+  // Допустим, тема светлая
+  let theme = "light";
+  // Если <body> содержит класс .dark-theme…
+  if (document.body.classList.contains("dark-theme")) {
+    // …тогда делаем тему тёмной
+    theme = "dark";
+  }
+  // После чего сохраняем выбор в localStorage
+  localStorage.setItem("theme", theme);
+});
+
+const btn_1 = document.querySelector(".igg");
+const img_1 = document.querySelector(".img_main");
+
+btn_1.addEventListener("click", () => {
+  img_1.src = "./assets/image/guest.png";
+});
